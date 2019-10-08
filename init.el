231d7e56ca3ce73277812a75accc8471bae8ceb9
Richard Hunter <rm.hunter@gmail.com>
2019-10-08 15:16:57 -0500

    some indentenation changes

--- init.el
+++ init.el
@@ -5,6 +5,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
 ;; Register the Melpa Emacs Package Repository
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
@@ -18,14 +22,17 @@
 ;; Make all "yes or no" prompts show "y or n" instead
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;; Make it so all '.js' files auto load 'rjsx-mode'
-(add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
+;; Make it so all '.js' files auto load 'js2-mode'
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 ;; Disable tab characters in indentation
 (setq-default indent-tabs-mode nil)
 
-;; Use 4 spaces when tabbing HTML elements
-(setq-default sgml-basic-offset 4)
+;; Use 2 spaces when tabbing HTML elements
+(setq-default sgml-basic-offset 2)
+
+;; Are we on a mac?
+(setq is-mac (equal system-type 'darwin))
 
 ;; Disable the *Messages* Buffer
 (setq-default message-log-max nil)
@@ -37,6 +44,25 @@
              (let ((buffer "*Completions*"))
                (and (get-buffer buffer)
                     (kill-buffer buffer)))))
+
+
+;; Remove tabs
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+;; Indent a region
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer.
+Including indent-buffer, which should not be called automatically on save."
+  (interactive)
+  (untabify-buffer)
+  (delete-trailing-whitespace)
+  (indent-buffer))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Custom Key Bindings ;;
@@ -73,6 +99,9 @@
 ;; Hint: Useful for auto-formatting the entire file by pressing (Ctrl+c -> TAB -> TAB)
 (global-set-key (kbd "C-c TAB") 'mark-whole-buffer)
 
+;; Install custom themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/") 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load Custom Files inside ~/.emacs.d/custom-lisp ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -83,3 +112,20 @@
                  ))
     (mapc load-it (directory-files dir nil "\\.el$"))))
 (load-directory "~/.emacs.d/custom-lisp")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("5c9bd73de767fa0d0ea71ee2f3ca6fe77261d931c3d4f7cca0734e2a3282f439" "8de599adee6766e541af4aa5f01ffc9cfbcbf94adc60a80ccb6caccb3fbceaa1" default)))
+ '(package-selected-packages
+   (quote
+    (markdown-preview-mode markdown-mode web-mode rjsx-mode jsx-mode docker-compose-mode dockerfile-mode yaml-mode vue-mode json-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(region ((t (:background "color-237")))))
